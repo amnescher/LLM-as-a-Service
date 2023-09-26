6eac43d04ee83232ecf56a441795519b64bec050
AmnEscherCloud <a.sabet@eschercloud.ai>
2023-09-26 11:33:42 +0000

    new frontend

--- Ray_demo/pages/1_chatbot.py
+++ Ray_demo/pages/1_chatbot.py
@@ -108,60 +108,8 @@ def get_collections():
     collections_data = response.json()
     # selected_collections = st.multiselect("Select Collections:", response)
     collections_list = collections_data.get("collections", [])
-    collections_list.append('all_documents')
     return collections_list
 
-<<<<<<< HEAD
-st.title('EscherCloud AI LLM service - Demo ')
-#st.image("Eschercloud.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
-
-if "messages" not in st.session_state.keys():
-        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-
-
-st.sidebar.button('Clear Documents', on_click=clear_document)
-st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
-#st.sidebar.markdown("<br>", unsafe_allow_html=True)
-st.sidebar.markdown("---")  
-st.sidebar.markdown("<br>", unsafe_allow_html=True)
-
-search_choice = st.sidebar.radio(options=["AI Assistance","Document Search"], label="Type of search")
-if search_choice == 'Document Search':
-    collection_list = get_collections()
-    selected_collection = st.sidebar.selectbox("select collection", collection_list)
-    if st.sidebar.button("Confirm"):
-        if selected_collection is not None:
-                    PORT_NUMBER = 8000
-                    mode = "Collection choice"
-                    POST_URL = f"http://localhost:{PORT_NUMBER}/predict/?text={selected_collection}&mode={mode}"
-                    response = requests.post(POST_URL)
-                    print('response collection:', response, response.content)
-                    st.sidebar.success(f"Collection {selected_collection} selected!")
-
-for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
-    
-if prompt := st.chat_input():#(disabled=not replicate_api):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.write(prompt)
-        
-if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                PORT_NUMBER = 8000
-                URL = f"http://localhost:{PORT_NUMBER}/predict/?text={prompt}&mode={search_choice}&messages={st.session_state.messages}"
-                response = requests.post(URL)
-                response = response.content.decode()
-                if response:
-                    placeholder = st.empty()
-                    full_response = response
-                    placeholder.markdown(full_response)
-        message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
-=======
 
 if "username" not in st.session_state or st.sidebar.button("Logout"):
     # Login form
@@ -301,4 +249,3 @@ else:
                 st.session_state.messages.append(message)
 # Adding New Chat button to the code
 # fixing logout button so when you log out to land to the first page.
->>>>>>> 5066fa06b5ff5000320b868bcef72c19bc910bc8
