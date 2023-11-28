5d6a6def6d759b0034a934a41c1f5d43628b3502
nilsbarring <n.barring@eschercloud.ai>
2023-11-28 21:41:55 +0000

    modified vdb functions to work with frontend, modified frontend

--- Ray_demo/llmAPI/API/app/routers/VDB_functions.py
+++ Ray_demo/llmAPI/API/app/routers/VDB_functions.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
-from app.models import VectorDBRequest
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Security
+from app.models import VectorDBRequest,LoginUser
+from app.depencencies.security import get_current_active_user
 from app.depencencies.security import get_current_active_user
 from app.database import User
 import requests
@@ -9,6 +10,10 @@ import secrets
 import zipfile
 from typing import Optional
 import pathlib
+from sqlalchemy.orm import Session
+from app.database import get_db
+import weaviate
+import logging
 
 # Load environment variables and setup
 current_path = pathlib.Path(__file__).parent
@@ -16,9 +21,21 @@ config_path = current_path.parent.parent.parent / 'cluster_conf.yaml'
 received_files_dir = os.path.join(current_path.parent.parent.parent, 'received_files')
 os.makedirs(received_files_dir, exist_ok=True)
 
+logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename="app.log",  # specify the file name if you want logging to be stored in a file
+            filemode="a",  # append to the log file if it exists
+        )
+
+logger = logging.getLogger(__name__)
+logger.propagate = True
+
 # Load configuration
 with open(config_path, "r") as file:
     config = yaml.safe_load(file)
+
+#weaviate_client = weaviate.Client("http://localhost:8080")
 
 Ray_service_URL = config.get("Ray_service_URL")
 router = APIRouter()
@@ -72,3 +89,103 @@ async def query_vectorDB(data: VectorDBRequest = Depends(),
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An unexpected error occurred: {e}")
+
+@router.post("/add_vdb_class/")
+async def add_vdb_class(
+    data: VectorDBRequest,
+    current_user: User = Depends(get_current_active_user)):
+
+    try:            
+            weaviate_client = weaviate.Client("http://localhost:8080")
+            prefix = current_user.username
+            cls = str(prefix) + "_" + str(data.class_name)
+            #class_description = str(description)
+            vectorizer = 'text2vec-transformers'
+            if cls is not None:
+                schema = {'classes': [ 
+                    {
+                            'class': str(cls),
+                            'description': 'normal description',
+                            'vectorizer': str(vectorizer),
+                            'moduleConfig': {
+                                str(vectorizer): {
+                                    'vectorizerClassName': False,
+                                    }
+                            },
+                            'properties': [{
+                                'dataType': ['text'],
+                                'description': 'the text from the documents parsed',
+                                'moduleConfig': {
+                                    str(vectorizer): {
+                                        'skip': False,
+                                        'vectorizePropertyName': False,
+                                        }
+                                },
+                                'name': 'page_content',
+                            },
+                            {
+                                'name': 'document_title',
+                                'dataType': ['text'],
+                            }],      
+                            },
+                ]}
+                weaviate_client.schema.create(schema)
+            else:
+                return {"error": "No class name provided"}
+    except Exception as e:
+        return {"error": str(e)}
+
+@router.post("/remove_vdb_class/")  
+async def delete_weaviate_class(data: VectorDBRequest,
+    current_user: User = Depends(get_current_active_user)):
+        try: 
+            weaviate_client = weaviate.Client("http://localhost:8080")
+            username = current_user.username
+            class_name = data.class_name
+            full_class_name = str(username) + "_" + str(class_name)
+            weaviate_client.schema.delete_class(full_class_name)
+
+        except Exception as e:
+            return {"error": str(e)}
+
+@router.post("/get_docs_in_class/")    
+async def query_weaviate_document_names(data: VectorDBRequest,
+    current_user: User = Depends(get_current_active_user)):
+    try:
+        weaviate_client = weaviate.Client("http://localhost:8080")
+        username = current_user.username
+        class_properties = ["document_title"]
+        class_name = data.class_name
+        #full_class_name = str(username) + "_" + str(class_name)
+        full_class_name = 'Admin' + "_" + str(class_name)
+        query = weaviate_client.query.get(full_class_name, class_properties)
+        print('the query', query)
+        query = query.do()
+
+        document_title_set = set()
+        documents = query.get('data', {}).get('Get', {}).get(str(full_class_name), [])
+        #print('the documents', documents)
+        for document in documents:
+            document_title = document.get('document_title')
+            if document_title is not None:
+                document_title_set.add(document_title)
+        return list(document_title_set)
+    
+    except Exception as e:
+            return {"error": str(e)}
+    
+@router.post("/get_classes/")  
+async def delete_weaviate_class(data: VectorDBRequest,
+    current_user: User = Depends(get_current_active_user)):
+    try:
+        weaviate_client = weaviate.Client("http://localhost:8080")
+        username = current_user.username
+        schema = weaviate_client.schema.get()
+        classes = schema.get('classes', []) 
+        prefix = str(username) + "_"
+        prefix = prefix.capitalize()
+        filtered_classes = [cls["class"].replace(prefix, "", 1) for cls in classes if cls["class"].startswith(prefix)] #[cls["class"] for cls in classes if cls["class"].startswith(prefix)]
+        return filtered_classes
+    
+    except Exception as e:
+            return {"error": str(e)}
--- Ray_demo/llmAPI/UI/pages/2_database_management.py
+++ Ray_demo/llmAPI/UI/pages/2_database_management.py
@@ -3,69 +3,104 @@ import streamlit as st
 import os
 import requests
 from langchain.document_loaders import YoutubeLoader
+import logging
+
+logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename="app.log",  # specify the file name if you want logging to be stored in a file
+            filemode="a",  # append to the log file if it exists
+        )
+
+logger = logging.getLogger(__name__)
+logger.propagate = True
 
 port = 5001
-pdf_directory = "./PDF_dir"
-if not os.path.exists(pdf_directory):
-    os.makedirs(pdf_directory)
-    print(f"Directory '{pdf_directory}' created successfully.")
+BASE_URL = "http://localhost:8083"
 
+def add_class(username, class_name, access_token):
+    params = {
+        "username": username,
+        "class_name": class_name
+        }
+    print('query data', params)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    resp = requests.post(f"{BASE_URL}/vector_DB_request/add_vdb_class/",json=params, headers=headers)
+    if resp.status_code == 200:
+        print(resp.status_code, resp.content)
+    else:
+        print(resp.status_code, resp.content)
 
-def upload_videos(user_input, port, collection, video_name):
-    try:
-        YoutubeLoader.from_youtube_url(user_input, add_video_info=True)
-        data = {"url": user_input}
-        PORT_NUMBER = 8000
-        Video_URL = user_input
-        data_type = "Video"
-        collection_ = collection
-        vid_name = video_name
-        mode = "add"
-        POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={Video_URL}&collection={collection_}&mode={mode}&doc_name={vid_name}"
-        response = requests.post(POST_URL)
-        print("the response upload video: ", response, response.content)
-        st.text("Videos Uploaded!" + " ✅")
-    except Exception as e:
-        st.warning("Please enter a valid YouTube video URL.")
+def delete_class(username, class_name, access_token):
+    params = {
+        "username": username,
+        "class_name": class_name
+        }
+    headers = {"Authorization": f"Bearer {access_token}"}
+    resp = requests.post(f"{BASE_URL}/vector_DB_request/remove_vdb_class/",json=params, headers=headers)
+    if resp.status_code == 200:
+        print(resp.status_code, resp.content)
+    else:
+        print(resp.status_code, resp.content)
 
+def display_documents(username, class_name, access_token):
+    params = {
+        "username": username,
+        "class_name": class_name
+        }
 
-def upload_webpage(user_input, port, collection, name):
-    data = {"url": user_input}
-    PORT_NUMBER = 8000
-    page_URL = user_input
-    data_type = "Webpage"
-    collection_ = collection
-    page_name = name
-    mode = "add"
-    POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={page_URL}&collection={collection_}&mode={mode}&doc_name={page_name}"
-    response = requests.post(POST_URL)
-    print("the response upload page: ", response, response.content)
-    st.text("Page Uploaded!" + " ✅")
+    #print("collection selected:", params)
 
+    headers = {"Authorization": f"Bearer {access_token}"}
+    resp = requests.post(f"{BASE_URL}/vector_DB_request/get_docs_in_class/",json=params, headers=headers)
+    
 
-def display_documents(selected_collections):
-    PORT = 8000
-    data_type = "Document"
-    mode = "get_all"
-    collection = selected_collections
+    #print("the response", resp, resp.content)
 
-    print("collection selected:", selected_collections)
-
-    POST_URL = f"http://localhost:{PORT}/VectoreDataBase/?data_type={data_type}&collection={collection}&mode={mode}"
-    response = requests.post(POST_URL)
-
-    print("the response", response, response.content)
-
-    response_content = response.content.decode("utf-8")
-    print("respcontent", response_content)
-    intermediate_data = json.loads(response_content)
-    print("inter", intermediate_data)
-    document_list = intermediate_data.get("collection", [])
+    response_content = resp.content.decode("utf-8")
+    #print("respcontent", response_content)
+    document_list = json.loads(response_content)
 
     return document_list
 
+def display_user_classes(username, access_token):
+    params = {
+        "username": username,
+        }
+
+    print("data:", params)
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    resp = requests.post(f"{BASE_URL}/vector_DB_request/get_classes/",json=params, headers=headers)
+    
+
+    print("the response", resp, resp.content)
+
+    response_content = resp.content.decode("utf-8")
+    print("respcontent", response_content)
+    user_classes = json.loads(response_content)
+    if resp.status_code == 200:
+        print(resp.status_code, resp.content)
+        return user_classes
+    else:
+        print(resp.status_code, resp.content)
+        return 
+    
+def authentication(username, password):
+    data = {"username": username, "password": password}
+    resp = requests.post(
+        f"{BASE_URL}/token", data=data
+    )
+    if "access_token" not in resp.json():
+        return None
+    return resp.json()["access_token"]
 
 def display_colleciton_in_table():
+    query_data = {
+        "data_type": "Collection", 
+        "mode": "get_all"
+        }
+    
     PORT = 8000
     data_type = "Collection"
     mode = "get_all"
@@ -90,133 +125,164 @@ st.header("Documents")
 
 col1, col2 = st.columns(2)
 PORT = 8000
-data_type = "Collection"
-mode = "get_all"
-POST_URL = f"http://localhost:{PORT}/VectoreDataBase/?data_type={data_type}&mode={mode}"
-response = requests.post(POST_URL)
+#data_type = "Collection"
+#mode = "get_all"
+#POST_URL = f"http://localhost:{PORT}/VectoreDataBase/?data_type={data_type}&mode={mode}"
+#response = requests.post(POST_URL)
 
-collections_data = response.json()
+#collections_data = response.json()
 
-collections_list = collections_data.get("collections", [])
-
-with col1:
-    collection_name = st.text_input("Enter collection name:")
-    collection_name = collection_name.strip().replace(" ", "-")
-    if st.button("Create Collection"):
-        if collection_name is not None:
-            print("col name", collection_name)
-            data_type = "Collection"
-            mode = "add"
-            PORT_NUMBER = 8000
-            POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={collection_name}&mode={mode}"
-            response = requests.post(POST_URL)
-            st.success(f"Collection {collection_name} added!")
-            documents = display_colleciton_in_table()
-
-    collection_to_delete = st.text_input("Collection to delete:")
-    if st.button("Delete Collection"):
-        if collection_to_delete is not None:
-            PORT_NUMBER = 8000
-            mode = "remove"
-            data_type = "Collection"
-            POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={collection_to_delete}&mode={mode}"
-            response = requests.post(POST_URL)
-            st.success(f"Collection {collection_to_delete} deleted!")
-            documents = display_colleciton_in_table()
-
-
-with col2:
-    # selected_collections = st.multiselect("Select Collections:", collections_list)
-    documents = display_colleciton_in_table()  # Fetch actual documents
-    st.table(documents)
-
-
-st.header("Select collections to use")
-selected_collections = st.selectbox("Select Collections:", documents)
-
-doc_list = display_documents(selected_collections)
-
-# Get the documents in collection:
-
-st.header("Collection selected", selected_collections)
-
-col1_2, col2_2 = st.columns(2)
-
-st.markdown("<br>", unsafe_allow_html=True)
-
-with col1_2:
-    management_mode = st.radio(
-        options=["Upload Document", "Upload Video", "Upload Webpage"],
-        label="Type upload",
-    )
-    if management_mode == "Upload Document":
-        st.subheader("Add Document")
-        uploaded_pdf = st.file_uploader("Add PDF", type=["pdf"])
-        document_name = st.text_input("Enter document name:")
-        document_metadata = st.text_input("Enter document metadata:")
-        if st.button("Upload document"):
-            if uploaded_pdf is not None:
-                pdf_filename = os.path.join(pdf_directory, uploaded_pdf.name)
-                # Save the uploaded PDF to the specified directory
-                with open(pdf_filename, "wb") as f:
-                    f.write(uploaded_pdf.read())
-                PORT_NUMBER = 8000
-                mode = "add"
-                data_type = "Document"
-                collection_ = selected_collections
-                doc_name = document_name
-                POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={pdf_filename}&mode={mode}&collection={collection_}&doc_name={doc_name}"
-                response = requests.post(POST_URL)
-                st.text("Document Uploaded!" + " ✅")
-
-                doc_list = display_documents(selected_collections)
-    elif management_mode == "Upload Video":
-        st.subheader("Upload Videos")
-        st.write("Enter YouTube video URLs below:")
-
-        video_name = st.text_input("name of the video")
-        user_input = st.text_input("Video URL:")
-
-        if st.button("Upload Videos"):
-            upload_videos(user_input, port, selected_collections, video_name)
-
-    elif management_mode == "Upload Webpage":
-        st.subheader("Upload Webpage")
-        st.write("Enter a webpage URLs below:")
-
-        page_name = st.text_input("name of the page")
-        url = st.text_input("page URL:")
-
-        if st.button("Upload Webpage"):
-            upload_webpage(url, port, selected_collections, page_name)
-
-    st.subheader("Remove Document")
-    selected_document = st.selectbox("Select document:", doc_list)
-    print("selected doc", selected_document)
-    if st.button("Remove document"):
-        if selected_document is not None:
-            PORT_NUMBER = 8000
-            mode = "remove"
-            data_type = "Document"
-            collection_rm = selected_collections
-            doc_rm_name = selected_document
-            print(
-                "collection name remove:",
-                collection_rm,
-                "doc name remove:",
-                doc_rm_name,
-                type(collection_rm),
-                type(doc_rm_name),
-            )
-            POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&mode={mode}&remove={collection_rm}&doc={doc_rm_name}"
-            response = requests.post(POST_URL)
-            print("Da resp", response, response.content)
-            st.text("Document Removed!" + " ✅")
-
-with col2_2:
-    doc_list = display_documents(selected_collections)
-    st.header("Available documents in the collection")
-    if doc_list:
-        st.table(doc_list)
+#collections_list = collections_data.get("collections", [])
+if "username" not in st.session_state or st.sidebar.button("Logout"):
+    if "username" not in st.session_state:
+        username = st.text_input("Enter your username:")
+        password = st.text_input('Enter your password', type='password') 
     else:
-        st.write("No documents to display")
+        username = st.session_state.username
+
+    if  st.button("Login"):  # Add password field
+                token =  authentication(username, password)
+                if token:
+                    st.session_state.token = token
+                    st.session_state.username = username
+                    st.session_state.show_logged_in_message = True
+                else:
+                    st.error("Invalid User")
+else:
+        if "show_logged_in_message" not in st.session_state:
+            st.session_state.show_logged_in_message = False
+
+        if st.session_state.show_logged_in_message:
+            logedin_username = st.session_state.username
+
+        with col1:
+            username = st.session_state.username
+            print('user', username)
+            st.header("user:", st.session_state.username)
+            class_name = st.text_input("Enter collection name:")
+            class_name = class_name.strip().replace(" ", "-")
+            if st.button("Create Collection"):
+                if class_name is not None:
+                    add_class(st.session_state.username, str(class_name), st.session_state.token)
+                    st.success(f"Collection {class_name} created!")
+                    #documents = display_colleciton_in_table()
+
+            collection_to_delete = st.text_input("Collection to delete:")
+            if st.button("Delete Collection"):
+                if collection_to_delete is not None:
+                    delete_class(st.session_state.username, str(collection_to_delete), st.session_state.token)
+                    st.success(f"Collection {collection_to_delete} deleted!")
+                    #documents = display_colleciton_in_table()
+                   # POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={collection_to_delete}&mode={mode}"
+                   # response = requests.post(POST_URL)
+                   # st.success(f"Collection {collection_to_delete} deleted!")
+                  #  documents = display_colleciton_in_table()
+
+
+        with col2:
+            classes = display_user_classes(st.session_state.username, st.session_state.token)
+            st.table(classes)
+            st.header("Select collections to use")
+            selected_collections = st.text_input("Select Collections:")
+            if st.button("Display class"):
+                if selected_collections is not None:
+                    doc_list = display_documents(st.session_state.username, str(selected_collections), st.session_state.token)
+                    print("doc list", doc_list)
+                    # selected_collections = st.multiselect("Select Collections:", collections_list)
+                    #documents = display_colleciton_in_table()  # Fetch actual documents
+                    st.table(doc_list)
+
+
+        st.header("Select collections to use")
+        #doc_list = display_documents(selected_collections)
+        #selected_collections = st.selectbox("Select Collections:", documents)
+
+      #  doc_list = display_documents(selected_collections)
+
+        # Get the documents in collection:
+
+       # st.header("Collection selected", selected_collections)
+
+'''''
+        col1_2, col2_2 = st.columns(2)
+
+        st.markdown("<br>", unsafe_allow_html=True)
+
+        with col1_2:
+            management_mode = st.radio(
+                options=["Upload Document", "Upload Video", "Upload Webpage"],
+                label="Type upload",
+            )
+            if management_mode == "Upload Document":
+                st.subheader("Add Document")
+                uploaded_pdf = st.file_uploader("Add PDF", type=["pdf"])
+                document_name = st.text_input("Enter document name:")
+                document_metadata = st.text_input("Enter document metadata:")
+                if st.button("Upload document"):
+                    if uploaded_pdf is not None:
+                        pdf_filename = os.path.join(pdf_directory, uploaded_pdf.name)
+                        # Save the uploaded PDF to the specified directory
+                        with open(pdf_filename, "wb") as f:
+                            f.write(uploaded_pdf.read())
+                        PORT_NUMBER = 8000
+                        mode = "add"
+                        data_type = "Document"
+                        collection_ = selected_collections
+                        doc_name = document_name
+                        POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&data_path={pdf_filename}&mode={mode}&collection={collection_}&doc_name={doc_name}"
+                        response = requests.post(POST_URL)
+                        st.text("Document Uploaded!" + " ✅")
+
+                        doc_list = display_documents(selected_collections)
+            elif management_mode == "Upload Video":
+                st.subheader("Upload Videos")
+                st.write("Enter YouTube video URLs below:")
+
+                video_name = st.text_input("name of the video")
+                user_input = st.text_input("Video URL:")
+
+                if st.button("Upload Videos"):
+                    upload_videos(user_input, port, selected_collections, video_name)
+
+            elif management_mode == "Upload Webpage":
+                st.subheader("Upload Webpage")
+                st.write("Enter a webpage URLs below:")
+
+                page_name = st.text_input("name of the page")
+                url = st.text_input("page URL:")
+
+                if st.button("Upload Webpage"):
+                    upload_webpage(url, port, selected_collections, page_name)
+
+            st.subheader("Remove Document")
+            selected_document = st.selectbox("Select document:", doc_list)
+            print("selected doc", selected_document)
+            if st.button("Remove document"):
+                if selected_document is not None:
+                    PORT_NUMBER = 8000
+                    mode = "remove"
+                    data_type = "Document"
+                    collection_rm = selected_collections
+                    doc_rm_name = selected_document
+                    print(
+                        "collection name remove:",
+                        collection_rm,
+                        "doc name remove:",
+                        doc_rm_name,
+                        type(collection_rm),
+                        type(doc_rm_name),
+                    )
+                    POST_URL = f"http://localhost:{PORT_NUMBER}/VectoreDataBase/?data_type={data_type}&mode={mode}&remove={collection_rm}&doc={doc_rm_name}"
+                    response = requests.post(POST_URL)
+                    print("Da resp", response, response.content)
+                    st.text("Document Removed!" + " ✅")
+
+        with col2_2:
+            doc_list = display_documents(selected_collections)
+            st.header("Available documents in the collection")
+            if doc_list:
+                st.table(doc_list)
+            else:
+                st.write("No documents to display")
+
+'''
