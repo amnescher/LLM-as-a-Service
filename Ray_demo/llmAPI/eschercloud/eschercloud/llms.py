7ecaaead7fa5a3d2c93f5e7f61627cef90a35376
AmnEscherCloud <a.sabet@eschercloud.ai>
2024-01-16 14:20:09 +0000

    API_test NoteBook

--- Ray_demo/llmAPI/eschercloud/eschercloud/llms.py
+++ Ray_demo/llmAPI/eschercloud/eschercloud/llms.py
@@ -3,10 +3,12 @@ from typing import TypedDict
 
 class InferenceRequest(TypedDict):
     prompt: str
-    newchat: bool
+    memory: bool
     conversation_number: int
     AI_assistance: bool
     collection_name: str
+    llm_model: str 
+
 
 
 
@@ -23,7 +25,7 @@ class llmService:
         if not self.access_token:
             raise Exception("Not authenticated, please set you access token using authenticate() method")
         headers = {"Authorization": f"Bearer {self.access_token}"}
-        resp = requests.post(f"{self.base_url}/llm_request", json=query_data, headers=headers)
+        resp = requests.post(f"{self.base_url}/llm_request/", json=query_data, headers=headers)
         if resp.status_code == 200:
             return resp.json()
         else:
@@ -38,3 +40,5 @@ class llmService:
             return resp.json()
         else:
             raise Exception("Database request failed")
+        
+
